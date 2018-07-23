04833dd879a614c378faeb7362b5b8ce1ec1ff94
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-07-23 13:34:04 -0500

    grinch

--- orgInit.sh
+++ orgInit.sh
@@ -19,13 +19,20 @@ sfdx force:user:create -f config/userDef/user12.json
 sfdx force:user:create -f config/userDef/user13.json
 sfdx force:user:create -f config/userDef/user14.json
 
-sfdx force:data:record:update -s User -w "FirstName='User' LastName='User'" -v "FirstName='Scott' LastName='Konkol' Department='Marketing'"
+sfdx force:data:record:update -s User -w "FirstName='User' LastName='User'" -v "FirstName='Awesome' LastName='Admin' Department='Marketing'"
 sfdx force:data:record:update -s User -w "FirstName='Security' LastName='User'" -v "FirstName='Angelos' LastName='Kottas' Department='Sales'"
 sfdx force:data:record:update -s User -w "FirstName='Integration' LastName='User'" -v "FirstName='Rachel' LastName='Beard' Department='Customer Support'"
+
+# deactivate the chatter expert user
+sfdx force:data:record:update -s User -w "LastName='Chatter Expert'" -v "isActive='false'"
 
 
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:user:permset:assign -n VolunteeringAppExtensions
 # sfdx force:data:tree:import -p data/masterImportPlan.json
 sfdx force:apex:execute -f scripts/setup.cls
+
+# one more user who doesn't volunteer
+sfdx force:user:create -f config/userDef/user15.json
+
 sfdx force:org:open
