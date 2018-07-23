0c5db97d4585f4edb0cd79698c62f4127dfd3acb
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-07-23 15:02:59 -0500

    moar users, department on report type

--- orgInit.sh
+++ orgInit.sh
@@ -18,14 +18,27 @@ sfdx force:user:create -f config/userDef/user11.json
 sfdx force:user:create -f config/userDef/user12.json
 sfdx force:user:create -f config/userDef/user13.json
 sfdx force:user:create -f config/userDef/user14.json
+sfdx force:user:create -f config/userDef/user16.json
+sfdx force:user:create -f config/userDef/user17.json
+sfdx force:user:create -f config/userDef/user18.json
+sfdx force:user:create -f config/userDef/user19.json
+sfdx force:user:create -f config/userDef/user20.json
+sfdx force:user:create -f config/userDef/user21.json
+sfdx force:user:create -f config/userDef/user22.json
+sfdx force:user:create -f config/userDef/user23.json
+sfdx force:user:create -f config/userDef/user24.json
+sfdx force:user:create -f config/userDef/user25.json
+sfdx force:user:create -f config/userDef/user26.json
+sfdx force:user:create -f config/userDef/user27.json
+sfdx force:user:create -f config/userDef/user28.json
+sfdx force:user:create -f config/userDef/user29.json
+sfdx force:user:create -f config/userDef/user30.json
+sfdx force:user:create -f config/userDef/user31.json
+sfdx force:user:create -f config/userDef/user32.json
 
 sfdx force:data:record:update -s User -w "FirstName='User' LastName='User'" -v "FirstName='Awesome' LastName='Admin' Department='Marketing'"
 sfdx force:data:record:update -s User -w "FirstName='Security' LastName='User'" -v "FirstName='Angelos' LastName='Kottas' Department='Sales'"
 sfdx force:data:record:update -s User -w "FirstName='Integration' LastName='User'" -v "FirstName='Rachel' LastName='Beard' Department='Customer Support'"
-
-# deactivate the chatter expert user
-sfdx force:data:record:update -s User -w "LastName='Chatter Expert'" -v "isActive='false'"
-
 
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:user:permset:assign -n VolunteeringAppExtensions
